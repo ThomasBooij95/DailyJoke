56fd8d0c18b3173f58208f408d075f6e000578c3
MarcSchotman <m_a_schotman@hotmail.com>
2020-04-09 23:23:25 +0200

    scraper

---
+++ getDailyJoke.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import numpy as np 
+
+def get_page_content():
+	#gets page content with 99 beautiful jokes hidden in the html soup
+	#Gives 0 for error
+	page = requests.get("https://www.boredpanda.com/funny-dad-jokes-puns/?utm_source=google&utm_medium=organic&utm_campaign=organic")
+	
+	if page.status_code == 200:
+		soup = BeautifulSoup(page.content, 'html.parser')
+		foundJokes = soup.find_all('p', 'post-content-description text-open-list')
+	else:
+		foundJokes = [0]
+
+	return list(foundJokes)
+
+def parse_jokes(foundJokes):
+	#parses jokes from the nasty html line where it was found
+	n=0
+	for joke in foundJokes:
+		joke = str(joke)
+		parsedJoke = joke.split(">")[2].split("<")[0]
+		foundJokes[n] = parsedJoke
+		n+=1
+		# print("Joke " , n,": ", splitJoke)
+	return foundJokes
+
+
+foundJokes = get_page_content()
+parsedJokes = parse_jokes(foundJokes)
+
+#prints all jokes
+for joke in parsedJokes:
+	print(joke)
