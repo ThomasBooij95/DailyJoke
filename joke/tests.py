a832f24111adea744625f2ce60aa70c5beae8e57
Thomas <thomasbooij95@gmail.com>
2020-05-01 13:21:17 +0200

    Add simple unit test to Joke model.

--- joke/tests.py
+++ joke/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Joke
 # Create your tests here.
+class JokeTestCase(TestCase):
+    def setUp(self):
+        Joke.objects.create(joke="Testjoke1")
+        print("running setup..")
+    def test_joke(self):
+        joke1 = Joke.objects.get(joke="Testjoke1")
+        self.assertEqual(joke1.joke,"Testjoke1")
