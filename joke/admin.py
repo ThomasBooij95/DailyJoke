e9f7f214c9f30dbc336909a78e38849516441834
Thomas <thomasbooij95@gmail.com>
2020-05-01 18:38:29 +0200

    Add comment functionality in the jokes admin.

--- joke/admin.py
+++ joke/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
  # Register your models here.
 from .models import Joke,Comment
+class CommentInline(admin.TabularInline):
+    model = Comment
 class JokeAdmin(admin.ModelAdmin):
+    inlines = [
+            CommentInline
+            ]
     pass
 class CommentAdmin(admin.ModelAdmin):
+
     exclude = ['pic_id']
 
 
