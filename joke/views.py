1fd20a2ed7cd1069f81cfd9faf5432052f4080ca
Thomas <thomasbooij95@gmail.com>
2020-05-01 18:20:05 +0200

    Squashed commit of the following:
    commit 1cb6a54085562369160842ca3013129533bfbfa5
    Author: Thomas <thomasbooij95@gmail.com>
    Date:   Fri May 1 18:13:07 2020 +0200
    
        Fix author featuring. Ready for release.
    
    commit 43aa95897637c74a667511110d665c777934adc6
    Author: Thomas <thomasbooij95@gmail.com>
    Date:   Fri May 1 17:54:43 2020 +0200
    
        Add testing to template
    
    commit a832f24111adea744625f2ce60aa70c5beae8e57
    Author: Thomas <thomasbooij95@gmail.com>
    Date:   Fri May 1 13:21:17 2020 +0200
    
        Add simple unit test to Joke model.
    
    commit 9b9f1bbe89ec26cabff719400af1e466c58782e4
    Author: Thomas <thomasbooij95@gmail.com>
    Date:   Fri May 1 01:12:17 2020 +0200
    
        Add author after joke if it exists.
    
    commit 71a6012464ed5db8fc6cc65383c531e51b983621
    Author: Thomas <thomasbooij95@gmail.com>
    Date:   Fri May 1 01:10:37 2020 +0200
    
        Add override functionality and use this for getting the joke
    
    commit cc393c5c903d982b053e8ca015f6b4cf72fd220b
    Author: Thomas <thomasbooij95@gmail.com>
    Date:   Fri May 1 01:09:22 2020 +0200
    
        Modify Admin panel to hide pic_id with comments.
    
    commit cca22562e82c41b95f199f3bfd2466df1d671a03
    Author: Thomas <thomasbooij95@gmail.com>
    Date:   Fri May 1 01:04:38 2020 +0200
    
        Add migrations for joke author and add author field to joke model

--- joke/views.py
+++ joke/views.py
@@ -17,6 +17,7 @@ def getContext(request):
     currentJoke = getCurrentJoke()
 
 
+
     if currentUser.is_anonymous:
         profile_pic = "joke/img/anonymous_dad.jpeg"
     else:
@@ -27,8 +28,15 @@ def getContext(request):
     #Check if user liked the joke, gives boolean 
     likedJoke = JokeLike.userLikedJoke(current_user = currentUser, current_joke = currentJoke)
     comments = getComments(currentJoke.id)
-
+    
     likes = countJokeLikes(currentJoke)
+    # Check whether the Joke has an author and give its username to context.
+    if(currentJoke.author):
+        authorJoke = currentJoke.author.username
+    else:
+        authorJoke = ''
+    
+
     # likedJoke = 
     context = {
         "joke" : currentJoke.joke,
@@ -36,13 +44,14 @@ def getContext(request):
         "comments" : comments,
         "likedJoke": likedJoke,
         "profile_pic": profile_pic,
-
+        "author": authorJoke
         }
     return context
 
 def home_view(request):
     context = getContext(request)
     return render(request, 'home.html', context)
+
 
 
 def getComments(jokeId):
@@ -68,9 +77,15 @@ def getJokeNumber():
     return jokeNr
 
 def getCurrentJoke():
+
     jokeNumber = getJokeNumber()
-    jokes = Joke.objects.all()
-    current_joke = jokes[jokeNumber]
+    jokes = Joke.objects.all().filter(override=True)
+    print(jokes)
+    if not jokes: # When no jokes are featured with the admin
+        jokes = Joke.objects.all()
+        current_joke = jokes[jokeNumber]
+    else:
+        current_joke = jokes[0]
     return current_joke
 
 def countJokeLikes(joke):
